74bfd48ec1369dea3c3ef60906d06856c742c4bb
Andreas Vieten <cryptonari@outlook.com>
2022-01-05 20:44:10 +0100

    Build out AccessControl/Role Contracts

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -5,5 +5,13 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     }
+  },
+  // Compiler configuration to match starter code contracts "pragma solidity ^0.4.23"
+  compilers: {
+    solc: {
+      version: "0.4.24",    // A version or constraint - Ex. "^0.5.0"
+                            // Can be set to "native" to use a native solc or
+                            // "pragma" which attempts to autodetect compiler versions
+    }
   }
 };
